bfb034a700a15200108a5ee4793cad7fd328332f
Niko Vrdoljak <niko.vrdoljak@hotmail.com>
2024-11-12 14:15:21 +0100

    dodan crud

--- app.py
+++ app.py
@@ -1,6 +1,106 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FileField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, RadioField, DateField, FileField
+from wtforms.validators import DataRequired, Length
+from flask_wtf.file import FileAllowed
+from datetime import datetime
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-@app.route("/")
+app = Flask(__name__)
+app.secret_key = 'tajni_ključ'
+bootstrap = Bootstrap5(app)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['temp_pzw_blog_database']
+posts_collection = db['posts']
+
+
+class NameForm(FlaskForm):
+    name = StringField("Ime", validators=[DataRequired()])
+    submit = SubmitField("Pošalji")
+
+class BlogPostForm(FlaskForm):
+    title = StringField('Naslov', validators=[DataRequired(), Length(min=5, max=100)])
+    content = TextAreaField('Sadržaj')
+    author = StringField('Autor', validators=[DataRequired()])
+    status = RadioField('Status', choices=[('draft', 'Skica'), ('published', 'Objavljeno')], default='draft')
+    date = DateField('Datum', default=datetime.today)
+    tags = StringField('Oznake')
+    image = FileField('Blog Image', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Samo slike!')])
+    submit = SubmitField('Spremi')
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h1>Hello 7a</h1>"
+    published_posts = posts_collection.find({"status": "published"}).sort('date', -1)
+    return render_template('index.html', posts = published_posts)
+
+
+@app.route('/blog/create', methods=["get", "post"])
+def post_create():
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        post = {
+            'title': form.title.data,
+            'content': form.content.data,
+            'author': form.author.data,
+            'status': form.status.data,
+            'date': datetime.combine(form.date.data, datetime.min.time()),
+            'tags': form.tags.data,
+            'date_created': datetime.utcnow()
+        }
+        posts_collection.insert_one(post)
+        flash('Post je uspješno upisan.', 'success')
+        return redirect(url_for('index'))
+    return render_template('blog_edit.html', form=form)
+
+@app.route('/blog/<post_id>')
+def post_view(post_id):
+    post = posts_collection.find_one({'_id': ObjectId(post_id)})
+
+    if not post:
+        flash("Post nije pronađen!", "danger")
+        return redirect(url_for('index'))
+
+    return render_template('blog_view.html', post=post)
+
+
+@app.route('/blog/edit/<post_id>', methods=["get", "post"])
+def post_edit(post_id):
+    form = BlogPostForm()
+    post = posts_collection.find_one({"_id": ObjectId(post_id)})
+
+    if request.method == 'GET':
+        form.title.data = post['title']
+        form.content.data = post['content']
+        form.author.data = post['author']
+        form.date.data = post['date']
+        form.tags.data = post['tags']
+        form.status.data = post['status']
+
+    elif form.validate_on_submit():
+        posts_collection.update_one(
+            {"_id": ObjectId(post_id)},
+            {"$set": {
+                'title': form.title.data,
+                'content': form.content.data,
+                'date': datetime.combine(form.date.data, datetime.min.time()),
+                'tags': form.tags.data,
+                'status': form.status.data,
+                'date_updated': datetime.utcnow()
+            }}
+        )
+        flash('Post je uspješno ažuriran.', 'success')
+        return redirect(url_for('post_view', post_id = post_id))
+    else:
+        flash('Dogodila se greška!', category='warning')
+    return render_template('blog_edit.html', form=form)
+
+@app.route('/blog/delete/<post_id>', methods=['POST'])
+def delete_post(post_id):
+    posts_collection.delete_one({"_id": ObjectId(post_id)})
+    flash('Post je uspješno obrisan.', 'success')
+    return redirect(url_for('index'))
